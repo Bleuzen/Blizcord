d701ed6b32ee2b575d38228b625b4685b5bb274d
Elias <supgesu@gmail.com>
2019-08-03 17:54:43 +0200

    comment build script

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Build with Java 8
+# NOTE: to use this build script, you need to have Maven, Ant, Proguard and Launch4j installed.
+# On Archlinux you can install them for example with yay:
+# yay -S maven ant proguard launch4j
+
+# As an alternative, you can use IntelliJ IDEA to build the jar. It comes with Maven and Ant. Build configs for it exist in this repo.
+
+
+# Force to build with Java 8
 #export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
 
 # Download dependencies with maven
