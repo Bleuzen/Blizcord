a03f8dfd489ed3a3237c6ee82ac55aa847486873
Elias <supgesu@gmail.com>
2019-08-03 17:39:00 +0200

    add exe build config, change ant classes dir, build final files into /build, change proguard config to work with newer Java versions

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build with Java 8
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
+#export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
 
 # Download dependencies with maven
 mvn dependency:copy-dependencies
