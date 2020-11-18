fffe89ece2849a9e56ca8f63b31f272be9db13d1
Elias <supgesu@gmail.com>
2020-11-18 11:05:36 +0100

    version 0.10.4

--- src/com/github/bleuzen/blizcord/Values.java
+++ src/com/github/bleuzen/blizcord/Values.java
@@ -4,7 +4,7 @@ public class Values {
 
 	static final boolean DEV = false;
 
-	public static final String BOT_VERSION = "0.10.3" 	+ (DEV ? "-dev" : "");
+	public static final String BOT_VERSION = "0.10.4" 	+ (DEV ? "-dev" : "");
 	public static final String BOT_NAME = "Blizcord";
 	public static final String BOT_DEVELOPER = "Bleuzen <supgesu@gmail.com>";
 	public static final String BOT_GITHUB_REPO = "Bleuzen/Blizcord";
